b890c2852e1ea3c1850290a9fd7edcaac4d87fb5
Ahmad-Reza <ahmadrezambit@gmail.com>
2021-12-27 00:14:57 +0530

    optise code and check diff test cases

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -9,12 +9,18 @@ public class BubbleSort {
 
     private static void bubbleSort(int[] arr) {
         for(int i = 0; i<arr.length; i++){
+            int flag = 0;
             for(int j = 0; j<arr.length - 1; j++) {
                 if(arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    flag = 1;
                 }
+            }
+
+            if(flag == 0) {
+                break;
             }
         }
 
@@ -24,7 +30,7 @@ public class BubbleSort {
         }
     }
     public static void main(String[] args) {
-        int arr[] = {36, 19, 29, 12, 5};
+        int arr[] = {2, 36, 19, 29, 12, 5};
 
         System.out.println("Before sort :");
         for(int i = 0; i<arr.length; i++) {
