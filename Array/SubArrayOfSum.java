06cedecbce25eaf6a1a213b8fb8d471bb29186f9
Ahmad-Reza <ahmadrezambit@gmail.com>
2022-01-10 23:01:32 +0530

    efficient way with time complexity 0(n)

--- Array/SubArrayOfSum.java
+++ Array/SubArrayOfSum.java
@@ -3,53 +3,24 @@ package Array;
 public class SubArrayOfSum {
 
     static int subArrayOfSum(int[] arr, int sum) {
-        // int res = 0;
-        // int startIndes = 0, endIndex = 0;
+        int res = 0;
 
-        // for(int i=0; i<arr.length; i++) {
-        //     res = arr[i];
-        //     for(int j=1; j<arr.length; j++) { 
-        //         res = res + arr[j];        
-                
-        //         if(res == sum) {
-        //             startIndes = i;
-        //             endIndex = j;
-
-        //             System.out.println(i +" "+ j);
-        //             System.out.println(startIndes +" "+ endIndex);
-        //         } 
-                
-        //         if(res > sum){
-        //             break;
-        //         }
-        //     }
-
-        //     if(res == sum) break;
-        // }
-
-        // System.out.println("Sum found between indexes " + startIndes + " and " + endIndex);
-        
-        int curr_sum, i, j;
- 
-        // Pick a starting point
-        for (i = 0; i < arr.length; i++) {
-            curr_sum = arr[i];
- 
-            // try all subarrays starting with 'i'
-            for (j = i + 1; j <= arr.length; j++) {
-                if (curr_sum == sum) {
-                    int p = j - 1;
-                    System.out.println(
-                        "Sum found between indexes " + i
-                        + " and " + p);
+        for(int i=0; i<arr.length; i++) {
+            res = arr[i];
+            for(int j=i+1; j<arr.length; j++) {                 
+                if(res == sum) {
+                    System.out.println("Sum found between indexes " + i + " and " + (j-1));
                     return 1;
-                }
-                if (curr_sum > sum || j == arr.length)
+                } 
+                
+                if(res > sum || j == arr.length){
                     break;
-                curr_sum = curr_sum + arr[j];
+                }
+
+                res = res + arr[j];        
             }
         }
- 
+
         System.out.println("No subarray found");
         return 0;
     }
@@ -58,6 +29,35 @@ public class SubArrayOfSum {
         int arr[] = {1, 4, 20, 3, 10, 5};
         int sum = 33;
 
-        subArrayOfSum(arr, sum);
+        // time complexity = 0(n^2);
+        // space complexity = 0(1) 
+        subArrayOfSum(arr, sum); 
+
+        // time complecity = 0(n)
+        // space complexity = 0(1)
+        subArraySum(arr, sum);
+    }
+
+    static int subArraySum(int[] arr, int sum) {
+        int i = 0, start = 0, tempSum = arr[0];
+        for(i=1; i<=arr.length; i++) {
+        
+            //tempSum will greater than sum then subtract first index value from tempSum and increase index
+            while(tempSum > sum && start < i-1) {
+                tempSum = tempSum - arr[start];
+                start++;
+            }
+
+            while(tempSum == sum) {
+                System.out.println("Sum found between indexes " + start + " and " + (i - 1));
+                return 1;
+            }
+
+            if(start < i)
+                tempSum = tempSum + arr[i];
+        }
+
+        System.out.println("No subarray found");
+        return 0;
     }
 }
